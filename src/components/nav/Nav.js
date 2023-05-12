13fef7d429e8c44d1d1c47421c4f3cfe0c7b5d5b
lorenzo-mm <cepo_mm@hotmail.com>
2023-05-12 11:30:26 +0200

    add new buttons for light/dark mode and lang

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,48 +3,63 @@ import './nav.css'
 import { AiOutlineHome } from 'react-icons/ai'
 import { BiUserPlus, BiBook, BiMessageSquareDetail } from 'react-icons/bi'
 
-import { BsFolder } from 'react-icons/bs'
+import { BsFolder, BsSun, BsMoon } from 'react-icons/bs'
 
 function Nav () {
   const [activeNav, setActiveNav] = useState('#')
+
   return (
-    <nav>
-      <a
-        href='#home'
-        onClick={() => setActiveNav('#home')}
-        className={activeNav === '#home' ? 'active' : ''}
-      >
-        <AiOutlineHome />
-      </a>
-      <a
-        href='#about'
-        onClick={() => setActiveNav('#about')}
-        className={activeNav === '#about' ? 'active' : ''}
-      >
-        <BiUserPlus />
-      </a>
-      <a
-        href='#experience'
-        onClick={() => setActiveNav('#experience')}
-        className={activeNav === '#experience' ? 'active' : ''}
-      >
-        <BiBook />
-      </a>
-      <a
-        href='#portfolio'
-        onClick={() => setActiveNav('#portfolio')}
-        className={activeNav === '#portfolio' ? 'active' : ''}
-      >
-        <BsFolder />
-      </a>
-      <a
-        href='#contact'
-        onClick={() => setActiveNav('#contact')}
-        className={activeNav === '#contact' ? 'active' : ''}
-      >
-        <BiMessageSquareDetail />
-      </a>
-    </nav>
+    <>
+      <nav>
+        <a
+          href='#home'
+          onClick={() => setActiveNav('#home')}
+          className={activeNav === '#home' ? 'active' : ''}
+        >
+          <AiOutlineHome />
+        </a>
+        <a
+          href='#about'
+          onClick={() => setActiveNav('#about')}
+          className={activeNav === '#about' ? 'active' : ''}
+        >
+          <BiUserPlus />
+        </a>
+        <a
+          href='#experience'
+          onClick={() => setActiveNav('#experience')}
+          className={activeNav === '#experience' ? 'active' : ''}
+        >
+          <BiBook />
+        </a>
+        <a
+          href='#portfolio'
+          onClick={() => setActiveNav('#portfolio')}
+          className={activeNav === '#portfolio' ? 'active' : ''}
+        >
+          <BsFolder />
+        </a>
+        <a
+          href='#contact'
+          onClick={() => setActiveNav('#contact')}
+          className={activeNav === '#contact' ? 'active' : ''}
+        >
+          <BiMessageSquareDetail />
+        </a>
+      </nav>
+      <div className='utilities'>
+        <a
+          href='#language'
+        >
+          <BsSun />
+        </a>
+        <a
+          href='#mode'
+        >
+          <BsMoon />
+        </a>
+      </div>
+    </>
   )
 }
 
