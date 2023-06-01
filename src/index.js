da1f32a5e5533b24ff5973a57aea0dc532608867
lorenzo-mm <cepo_mm@hotmail.com>
2023-06-01 16:13:27 +0200

    add new lang components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+import es from './lang/es/global.json'
+import en from './lang/en/global.json'
+import i18next from 'i18next'
+import { I18nextProvider } from 'react-i18next'
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      global: en
+    },
+    es: {
+      global: es
+    }
+  }
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <I18nextProvider i18n={i18next}>
+    <App />
+  </I18nextProvider>
+)
